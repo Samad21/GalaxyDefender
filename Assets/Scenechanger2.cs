4b9bc1909dc920019aa6ee96e4b9efaf20e5ecb4
samad21 <SH1949@live.mdx.ac.uk>
2019-12-09 18:24:54 +0000

    New game modes
    New menu buttons added which lead to new games modes such as the hard mode and the hyperspeed mode, the audio for the level has been altered so it is not the same music all throughout the game.

---
+++ Assets/Scenechanger2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechanger2 : MonoBehaviour
+{
+    public void GotoHyperspeedScene()
+    {
+        SceneManager.LoadScene("Hyperspeed mode");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Hyperspeed mode");
+    }
+}
---
+++ Assets/Scenechanger3.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechanger3 : MonoBehaviour
+{
+    public void GotoHardModeScene()
+    {
+        SceneManager.LoadScene("HardMode");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("HardMode");
+    }
+}
