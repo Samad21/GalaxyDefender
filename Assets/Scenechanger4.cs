81651aba4a802b9c3177e6cdd330d37d44fcb66c
samad21 <SH1949@live.mdx.ac.uk>
2019-12-09 20:14:43 +0000

    Exit buttons
    A quit button was added on each game mode scene which will navigate the player back to the main menu scene, this is so the player can easily go through all the various game modes.

---
+++ Assets/Scenechanger4.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechanger4 : MonoBehaviour
+{
+    public void GotoMMenuScene()
+    {
+        SceneManager.LoadScene("MMenu");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
